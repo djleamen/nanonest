0f4cc1e18ff13eb9f5d3a4354607417e046c3a4e
Ellie-Cunningham <elliecunningham627@gmail.com>
2025-01-20 10:38:03 -0500

    Added comments to file parsing code

--- main.java
+++ main.java
@@ -8,19 +8,22 @@ public class main {
     HashMap<Integer, ArrayList<String>> map = new HashMap<>();
 
     try {
+      // Initialize File scanner
       File dataSet = new File("Air_Quality.csv");
       Scanner fileScanner = new Scanner(dataSet);
-      fileScanner.nextLine();
+      fileScanner.nextLine(); // Skips over first line with column labels, should be changed later
 
-      while(fileScanner.hasNext()) {
+      while(fileScanner.hasNext()) { // For each line in the csv
         String line = fileScanner.nextLine();
-        String splitLine[] = line.split(",");
+        String splitLine[] = line.split(","); // Columns split into string array
 
+        // Then placed into list
         ArrayList<String> temp = new ArrayList<>();
         for(int i = 1; i < splitLine.length; i++) {
           temp.add(splitLine[i]);
         }
 
+        // And finally added to the hashmap
         map.put(Integer.parseInt(splitLine[0]), temp);
       }
 
@@ -29,9 +32,5 @@ public class main {
     catch (Exception e) {
       e.printStackTrace();
     }
-
-    System.out.println(map.size());
-
-
   }
 }
