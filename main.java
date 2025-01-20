e22884b3595e6d3132dc506d0a822dcab460ec95
lucario622 <58982101+lucario622@users.noreply.github.com>
2025-01-20 10:42:36 -0500

    frontend

--- main.java
+++ main.java
@@ -1,9 +1,11 @@
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class main {
+  public static Scanner s;
   public static void main (String[] args) {
     HashMap<Integer, ArrayList<String>> map = new HashMap<>();
 
@@ -32,5 +34,68 @@ public class main {
     catch (Exception e) {
       e.printStackTrace();
     }
+
+    s = new Scanner(System.in);
+    Boolean running = true;
+    while (running) {
+        
+        System.out.println("1. Display all Entries");
+        System.out.println("2. Edit an entry");
+        System.out.println("3. Add an entry");
+        System.out.println("4. View Specific Entry");
+        System.out.println("5. Exit");
+        System.out.print("Input: ");
+
+        String inp = s.nextLine();
+        switch (inp) {
+            case "1":
+                displayEntries(map);
+                break;
+            case "2":
+                // editEntry();
+                break;
+            case "3":
+                // addEntry();
+                break;
+            case "4":
+                viewEntry(map);
+                break;
+            case "5":
+                running = false;
+                break;
+            default:
+                break;
+        }
+    }
+    s.close();
   }
+
+  public static void displayEntries(Map<Integer, ArrayList<String>> map) {
+        System.out.println();
+        for (Map.Entry<Integer, ArrayList<String>> entry : map.entrySet()) {
+            Int key = entry.getKey();
+            ArrayList<String> value = entry.getValue();
+            System.out.println(key + ": " + value);
+        }
+        System.out.println();
+    }
+
+    public static void viewEntry(Map<Integer, ArrayList<String>> map) {
+        System.out.print("Choose Entry by ID: ");
+        String inp = s.nextLine();
+        if (map.containsKey(Integer.parseInt(inp))) {
+            ArrayList<String> value = map.get(Integer.parseInt(inp));
+            System.out.println(inp + ": " + value);
+        }
+    }
+
+    public static void editEntry(Map<Integer, ArrayList<String>> map) {
+        System.out.print("Choose Entry by ID: ");
+        String inp = s.nextLine();
+        if (map.containsKey(Integer.parseInt(inp))) {
+            ArrayList<String> value = map.get(Integer.parseInt(inp));
+
+        }
+
+    }
 }
