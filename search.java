f662a40c6b204f0e95a015aa964bde4558b59913
Nathaniel White <nathaniel.white3@ontariotechu.net>
2025-01-20 10:30:35 -0500

    Search function

---
+++ search.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+import java.util.*;
+import java.lang.*;
+
+public class search{
+    public static int searchHash(HashMap<String, Integer> map, String key){
+        return map.get(key);
+    }
+}
