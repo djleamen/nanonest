f64e4c5ef272e45f674b19cda9716342e70f324a
DylanJReynolds <dylan.reynolds@ontariotechu.net>
2025-03-27 10:46:22 -0400

    Created Integration and System Test Template

---
+++ src/test/java/furnitureCatalogue/IntegrationTests.java
@@ -0,0 +1,51 @@
+package furnitureCatalogue;
+
+import org.junit.jupiter.api.*;
+import java.io.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class IntegrationTests {
+
+    @Test
+    public void testSystemEntry() {
+
+    }
+
+    @Test
+    public void testDisplayedEntries() {
+
+    }
+
+    @Test
+    public void testEditUI() {
+
+    }
+
+    @Test
+    public void testRemoveUI() {
+
+    }
+
+    @Test
+    public void testAddUI() {
+
+    }
+
+    @Test
+    public void testAdminCredentials() {
+
+    }
+
+    @Test
+    public void testUserCredentials() {
+
+    }
+
+    @Test
+    public void testSearchUI() {}
+
+    @Test
+    public void testSystemRunThrough() {
+
+    }
+}
