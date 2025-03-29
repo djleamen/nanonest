9d6df0b82d02a33dd5244c4064c44463df04188e
Ellie-Cunningham <elliecunningham627@gmail.com>
2025-03-29 15:28:29 -0400

    General improvements and optimizations to relevancy search features. Light cleanup of SearchModel class.

--- src/main/java/furnitureCatalogue/SearchPackage/SearchModel.java
+++ src/main/java/furnitureCatalogue/SearchPackage/SearchModel.java
@@ -30,10 +30,12 @@ public class SearchModel {
         return m;
     }
 
-    public void query() {
+    // Runs entire search routine, should only ever be run from searchController.
+    protected void query() {
         String url = "src/main/resources/" + fileName;
         String order = controller.sortMode ? " ASC" : " DESC";
-        String query = controller.query;
+        String query = controller.query.replace(" ","%");
+        // Filter requires additional formatting. Filters are used to build chunk of an sql query
         String filter = "";
         for (String s : controller.filters.keySet()) {
             filter += s + " = '" + controller.filters.get(s) + "' AND ";
@@ -42,21 +44,21 @@ public class SearchModel {
             filter += s + " BETWEEN " + controller.ranges.get(s).get(0)
                     + " AND " + controller.ranges.get(s).get(1) + " AND ";
         }
-        // Remove trailing " AND "
+
+        // Fix any lingering formatting issues from concatenations
         if(!filter.isEmpty()) {
             filter = filter.substring(0, filter.length() - 4);
             filter = "WHERE " + filter;
         }
 
+        // Database is created locally in memory only reading from the main csv
         try (Connection connection = DriverManager.getConnection("jdbc:h2:mem:")) {
+            // Load main csv into table
             PreparedStatement load = connection.prepareStatement(
                     "CREATE TABLE t AS SELECT * FROM CSVREAD('" + url + "')");
             load.execute();
 
-            String format = formatQuery(query, connection);
-//            PreparedStatement search = connection.prepareStatement(format);
-//            ResultSet queryStepOne = search.executeQuery();
-
+            // Integer columns are by default loaded as strings, this converts them back to integers
             PreparedStatement reformat = connection.prepareStatement(
                     "ALTER TABLE t ALTER COLUMN id INTEGER;" +
                     "ALTER TABLE t ALTER COLUMN Quantity INTEGER;" +
@@ -64,11 +66,13 @@ public class SearchModel {
                     "ALTER TABLE t ALTER COLUMN Price INTEGER;");
             reformat.execute();
 
+            // Main query that generates final result
+            String format = formatQuery(query);
             PreparedStatement searchFilter = connection.prepareStatement("(" + format +
                     ") INTERSECT SELECT * FROM t " + filter + "ORDER BY " + controller.sortCategory + order);
-//            searchFilter.setString(1, query);
             ResultSet queryResult = searchFilter.executeQuery();
 
+            // Output results
             List<String> headers = Arrays.asList("Name", "Price", "Furniture Type", "Colour",
                                                    "Materials", "Size", "Quantity", "Company", "Style", "Weight");
             while (queryResult.next()) {
@@ -89,8 +93,9 @@ public class SearchModel {
         }
     }
 
-    private String formatQuery(String query, Connection conn) throws SQLException {
-        int depth = (query.length()/3) + 1;
+    // Generates chunk of sql query that allows for some typos in search entry
+    private String formatQuery(String query) {
+        int depth = (query.length()/3) + 1; // Represents how many wrong characters are permitted
         String formattedQuery = "SELECT * FROM t WHERE Name LIKE '%" + query + "%'";
         if(query.length() < 4) {
             return formattedQuery;
@@ -98,30 +103,33 @@ public class SearchModel {
         else if(query.length() == 6) {
             depth = 2;
         }
+        else if(query.length() > 9) {
+            depth = 3;
+        }
 
-        List<String> permutations = recursiveFormatQuery(new ArrayList<>(), query, depth, conn);
+        List<String> permutationsBase = recursiveFormatQuery(query, depth); // Recursive function
+        // Many results from recursion are unnecessary duplicates, this filters them out.
+        List<String> permutations = new ArrayList<>();
+        for(String s : permutationsBase) {
+            if(!permutations.contains(s)) {
+                permutations.add(s);
+            }
+        }
 
+        // Building the sql chunk
         for(String s : permutations) {
+            System.out.println(s);
             formattedQuery += " UNION SELECT * FROM t WHERE Name LIKE '%" + s + "%'";
         }
-//        System.out.println(formattedQuery);
         return formattedQuery;
     }
 
-    private List<String> recursiveFormatQuery (List<String> permutations, String query, int depth, Connection conn) throws SQLException {
-//        System.console().printf(query + "\n");
-//        System.console().printf(depth + "\n");
-
-//        formattedQuery += " UNION SELECT * FROM t WHERE Name LIKE '%" + query + "%'";
-
+    // Recursively adds different combinations of wildcards and stores them in a list
+    private List<String> recursiveFormatQuery (String query, int depth) {
+        List<String> permutations = new ArrayList<>();
         if(depth > 0) {
             for (int i = 0; i < query.length(); i++) {
-                List<String> newPermutations = recursiveFormatQuery(permutations, query.substring(0, i) + "_" + query.substring(i + 1), depth - 1, conn);
-                for(String s : newPermutations) {
-                    if(!(permutations.contains(s))) {
-                        permutations.add(s);
-                    }
-                }
+                permutations.addAll(recursiveFormatQuery(query.substring(0, i) + "_" + query.substring(i + 1), depth - 1));
             }
         }
         else {
